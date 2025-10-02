17bcb21e1277f1cdbd30248744b6a523d212a0d9
anthonyde laverde rios <anthonylaverde1994@gmail.com>
2025-10-02 14:23:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2421163979-3f577",
+  "appId": "1:948939084179:web:497ca5366fc5749975d866",
+  "apiKey": "AIzaSyDnlJZymM2xhxqKk5p_hARj0XtW7oUFPTc",
+  "authDomain": "studio-2421163979-3f577.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "948939084179"
+};
