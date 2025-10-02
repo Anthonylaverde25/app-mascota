3a87b5704d80ec7c937d87ec3a4e5e36303d1adf
anthonyde laverde rios <anthonylaverde1994@gmail.com>
2025-10-02 11:15:24 +0000

    ReferenceError: React is not defined

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Bell, Bug, HeartPulse, Pill, Syringe } from 'lucide-react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Switch } from '@/components/ui/switch';
